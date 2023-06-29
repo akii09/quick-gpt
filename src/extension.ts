9f7c4dbcce1ace258e85456ca3c5e45f363c2a6f
akii09 <akashpise588@gmail.com>
2023-06-30 02:25:05 +0530

    code optimization

--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,17 @@
 import * as vscode from 'vscode';
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai';
 
 export function activate(context: vscode.ExtensionContext) {
   console.log('"Quick GPT" extension is now active!');
+
   const config = vscode.workspace.getConfiguration();
-  const openApiKey: string | undefined = config.get("quickGPT.apiKey");
-  console.log( openApiKey, 'aaaaa');
-  
+  const openApiKey: string | undefined = config.get('quickGPT.apiKey');
+
   if (!openApiKey) {
-    vscode.window.showErrorMessage(
-      'No OpenAI API key provided. The extension will not function without an API key.'
-    );
+    vscode.window.showErrorMessage('No OpenAI API key provided. The extension will not function without an API key.');
     return;
-    // vscode.window
-    //   .showInputBox({
-    //     prompt: 'Enter your OpenAI API key',
-    //     ignoreFocusOut: true,
-    //   })
-    //   .then((apiKeyInput) => {
-    //     if (!apiKeyInput) {
-    //       vscode.window.showErrorMessage(
-    //         'No OpenAI API key provided. The extension will not function without an API key.'
-    //       );
-    //       return;
-    //     }
-    //     openApiKey = apiKeyInput;
-    //     context.secrets.store(API_KEY_STORAGE_KEY, openApiKey);
-    //   });
   }
-  // Code explanation command
+
   let disposeExplanation = vscode.commands.registerCommand('extension.explain_quickGPT', () => {
     const editor = vscode.window.activeTextEditor;
     if (editor) {
@@ -42,9 +25,7 @@ export function activate(context: vscode.ExtensionContext) {
       vscode.window.showInformationMessage('Please open a file before using Quick GPT.');
     }
   });
-  
 
-  // Code error finding command
   let disposeErrorFinder = vscode.commands.registerCommand('extension.error_quickGPT', () => {
     const editor = vscode.window.activeTextEditor;
     if (editor) {
@@ -63,37 +44,37 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(disposeErrorFinder);
 }
 
-async function showGPTResults(codeSnippet: string, type: string, key:string) {
+async function showGPTResults(codeSnippet: string, type: string, apiKey: string) {
   const outputChannel = vscode.window.createOutputChannel('Quick GPT');
   outputChannel.show();
   outputChannel.appendLine('Searching for code...');
-  // vscode.window.showInformationMessage('key-'+key);
+
   try {
-    
-    if (type==='error'){
-      codeSnippet = 'Please identify error of given code and resolve it \n' + codeSnippet;
-    }else if(type==='explain'){
-      codeSnippet = 'Please explain the given code \n' + codeSnippet;
+    if (type === 'error') {
+      codeSnippet = 'Please identify the error of the given code and resolve it\n' + codeSnippet;
+    } else if (type === 'explain') {
+      codeSnippet = 'Please explain the given code briefly\n' + codeSnippet;
     }
 
     const configuration = new Configuration({
-      apiKey: key,
+      apiKey: apiKey,
     });
     const openai = new OpenAIApi(configuration);
-    const response:any = await openai.createCompletion({
-      model: "text-davinci-003",
+    const response: any = await openai.createCompletion({
+      model: 'text-davinci-003',
       prompt: codeSnippet,
       max_tokens: 2500,
       temperature: 0,
     });
 
-    let result:any = '';
+    let result: any = '';
     if (response.data.choices.length) {
       result = response.data.choices[0].text;
     }
+
     if (result) {
       const outputChannel = vscode.window.createOutputChannel('Quick GPT');
-      outputChannel.appendLine(`Search results:\n`);
+      outputChannel.appendLine('Search results:');
       outputChannel.appendLine(result);
       outputChannel.show();
     } else {
